06e8cf83079b69386792bed671f59ef4812cad8a
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2020-08-18 02:15:03 +0200

    Output Users

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const main = document.getElementById('main');
 const addUserBtn = document.getElementById('add-user');
 const doubleBtn = document.getElementById('double');
-const showMillionairesBtn = document.getElementById('show-millionairs');
+const showMillionairesBtn = document.getElementById('show-millionaires');
 const sortBtn = document.getElementById('sort');
 const calculateWealthBtn = document.getElementById('calculate-wealth');
 
@@ -28,4 +28,28 @@ async function getRandomUser() {
 // Add new object to data array
 function addData(obj) {
   data.push(obj);
+  updateDOM();
 }
+
+// Update DOM
+function updateDOM(providedData = data) {
+  // Clear main div
+  main.innerHTML = '<h2><strong>Person</strong>Wealth</h2>';
+
+  providedData.forEach((item) => {
+    const element = document.createElement('div');
+    element.classList.add('person');
+    element.innerHTML = `<strong>${item.name}</strong>${formatMoney(
+      item.money
+    )}`;
+    main.appendChild(element);
+  });
+}
+
+// Format number as money
+function formatMoney(number) {
+  return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+// Event listners
+addUserBtn.addEventListener('click', getRandomUser);
