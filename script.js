a20432d30cbe51568369dc57dba1fca65a844609
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2020-08-19 01:21:29 +0200

    Calculate Wealth

--- script.js
+++ script.js
@@ -37,6 +37,18 @@ function showMillionaires() {
   data = data.filter((user) => user.money > 1000000);
   updateDOM();
 }
+// Calculate the total wealth
+function calculateWealth() {
+  const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+  const wealthEl = document.createElement('div');
+  wealthEl.innerHTML = `
+      <h3>
+      Total Wealth: <strong> ${formatMoney(wealth)}</strong>  
+      </h3>
+  `;
+
+  main.appendChild(wealthEl);
+}
 
 // Add new object to data array
 function addData(obj) {
@@ -79,3 +91,4 @@ doubleBtn.addEventListener('click', () => {
 
 sortBtn.addEventListener('click', sortByRichest);
 showMillionairesBtn.addEventListener('click', showMillionaires);
+calculateWealthBtn.addEventListener('click', calculateWealth);
